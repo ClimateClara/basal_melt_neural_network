b59be8b79b1e583ee3da713461540898dae22b00
Clara Burgard <clara.burgard@gmail.com>
2022-09-08 18:04:35 +0200

    job scripts for cross validatiojn

---
+++ scripts/BASH/JOB_SCRIPTS/large_extrap_std_noisf00_notblock09.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/run_cross_validation_NN.py large 09 00 extrap std 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/large_extrap_std_noisf00_notblock09.log
---
+++ scripts/BASH/JOB_SCRIPTS/large_extrap_std_noisf54_notblock00.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/run_cross_validation_NN.py large 00 54 extrap std 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/large_extrap_std_noisf54_notblock00.log
