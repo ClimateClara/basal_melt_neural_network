484c0f1dfaf4aeee481b375cbc070b1dfc8113df
Clara Burgard <clara.burgard@gmail.com>
2022-10-19 18:13:05 +0200

    sent away CV for most configs for whole profile

---
+++ scripts/BASH/JOB_SCRIPTS/medium_whole_std_noisf11_notblock00.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/run_cross_validation_NN.py medium 00 11 whole std 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/medium_whole_std_noisf11_notblock00.log
---
+++ scripts/BASH/JOB_SCRIPTS/medium_whole_std_noisf33_notblock00.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/run_cross_validation_NN.py medium 00 33 whole std 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/medium_whole_std_noisf33_notblock00.log
