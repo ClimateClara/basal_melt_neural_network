d042379b47d64ff47fbce4d5877af837a0832ee2
Clara Burgard <clara.burgard@gmail.com>
2022-10-31 14:17:25 +0100

    extrapolated T and S to bathymetry and prepared into format to be fed to NN

--- basal_melt_neural_networks/prep_input_data.py
+++ basal_melt_neural_networks/prep_input_data.py
@@ -538,3 +538,128 @@ def prepare_latlon_data_CV(tblock_dim, isf_dim, tblock_out, isf_out, inputpath_d
     var_val_norm = (val_input_df - var_mean)/var_range
     
     return summary_ds_all, var_train_norm, var_val_norm
+
+def prepare_input_data_addvar_CV(tblock_dim, isf_dim, tblock_out, isf_out, TS_opt, inputpath_data, 
+                                 name_extension, var_to_drop):
+
+    """
+    Computes normalisation metrics and normalised predictors and target for cross-validation.
+    
+    Parameters
+    ----------
+    tblock_dim : list
+        List of all time blocks to conduct the cross-validation on.
+    isf_dim : list
+        List of all ice shelves to conduct the cross-validation on.
+    tblock_out : int
+        Time block to leave out in cross-validation.
+    isf_out : list
+        Ice shelf to leave out in cross-validation.
+    TS_opt : str
+        Type of input temperature and salinity profiles to use. Can be 'extrap', 'whole', 'thermocline'
+    inputpath_data : str
+        Path to folder where to find the preformatted csv files.
+
+    Returns
+    -------
+    summary_ds_all: xr.Dataset
+        Dataset containing mean and denominator of the normalisation.
+    var_train_norm: xr.Dataset
+        Dataset containing normalised training predictors and target.
+    var_val_norm: xr.Dataset
+        Dataset containing normalised validation predictors and target.
+    """
+
+    ## are we dealing with leave-one-out cross-validation over time blocks?
+    tblock_list = list(tblock_dim)
+    if tblock_out > 0:
+        tblock_list.remove(tblock_out)
+    #print(tblock_list)
+
+    ## are we dealing with leave-one-out cross-validation over ice shelves?
+    isf_list = list(isf_dim)
+    if isf_out > 0:
+        isf_list.remove(isf_out)
+    
+    ## which profile option are we using for temperature and salinity
+    if TS_opt == 'extrap':
+        inputpath_prof = inputpath_data+'EXTRAPOLATED_ISFDRAFT_CHUNKS/'
+    elif TS_opt == 'whole':
+        inputpath_prof = inputpath_data+'WHOLE_PROF_CHUNKS/'
+    elif TS_opt == 'thermocline':
+        inputpath_prof = inputpath_data+'THERMOCLINE_CHUNKS/'
+
+    ### prepare training dataset
+
+    train_input_df = None        
+
+    for tt in tblock_list:
+
+        for kisf in isf_list: 
+
+            clean_df_nrun_kisf = pd.read_csv(inputpath_prof + 'dataframe_'+name_extension+'_isf'+str(kisf).zfill(3)+'_'+str(tt).zfill(3)+'.csv',index_col=[0,1,2])
+            clean_df_nrun_kisf.reset_index(drop=True, inplace=True)
+            clean_ds_nrun_kisf = clean_df_nrun_kisf.to_xarray()
+
+            if train_input_df is None:
+                train_input_df = clean_ds_nrun_kisf.copy()
+            else:
+                new_index = clean_ds_nrun_kisf.index.values + train_input_df.index.max().values+1
+                clean_ds_nrun_kisf = clean_ds_nrun_kisf.assign_coords({'index': new_index})
+                train_input_df = xr.concat([train_input_df, clean_ds_nrun_kisf], dim='index') 
+
+    ## prepare validation dataset
+
+    if (tblock_out > 0) and (isf_out == 0):  
+        tt_val = [tblock_out]
+        isf_val = isf_list
+    elif (isf_out > 0) and (tblock_out == 0):
+        isf_val = [isf_out]
+        tt_val = tblock_list
+    else:
+        print("I don't know how to handle leave ice shelves AND time blocks out, please teach me!")
+
+    val_input_df = None        
+
+    for tt in tt_val:
+
+        for kisf in isf_val: 
+
+            clean_df_nrun_kisf = pd.read_csv(inputpath_prof + 'dataframe_'+name_extension+'_isf'+str(kisf).zfill(3)+'_'+str(tt).zfill(3)+'.csv',index_col=[0,1,2])
+            clean_df_nrun_kisf.reset_index(drop=True, inplace=True)
+            clean_ds_nrun_kisf = clean_df_nrun_kisf.to_xarray()
+
+            if val_input_df is None:
+                val_input_df = clean_ds_nrun_kisf.copy()
+            else:
+                new_index = clean_ds_nrun_kisf.index.values + val_input_df.index.max().values+1
+                clean_ds_nrun_kisf = clean_ds_nrun_kisf.assign_coords({'index': new_index})
+                val_input_df = xr.concat([val_input_df, clean_ds_nrun_kisf], dim='index') 
+
+    ## prepare input and target
+
+    x_train = train_input_df.drop_vars([var_to_drop])
+    y_train = train_input_df[var_to_drop]
+    
+    x_val = val_input_df
+
+    #print('x_train : ',dfmt.print_shape_xr_ds(x_train), 'y_train : ',len(y_train))
+    #print('x_val  : ',dfmt.print_shape_xr_ds(x_val),  'y_test  : ',len(y_val))
+
+    ## normalise
+    norm_summary_list = []
+
+    for norm_method in ['std','interquart','minmax']:
+
+        summary_ds = compute_norm_metrics(x_train, y_train, norm_method)
+        norm_summary_list.append(summary_ds)
+
+    summary_ds_all = xr.concat(norm_summary_list, dim='norm_method')
+    
+    var_mean = summary_ds_all.sel(metric='mean_vars')
+    var_range = summary_ds_all.sel(metric='range_vars')
+
+    var_train_norm = (train_input_df - var_mean)/var_range
+    var_val_norm = (val_input_df - var_mean)/var_range
+    
+    return summary_ds_all, var_train_norm, var_val_norm
