d36d68dca7f9f9b18d50ff0fac6d286c9720e9f2
Clara Burgard <clara.burgard@gmail.com>
2022-11-17 17:35:10 +0100

    bash scripts of a few experiments

---
+++ scripts/BASH/JOB_SCRIPTS/experiments/extra_large_extrap_std_noisf00_notblock10_TSdraftslopereldGL.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/run_cross_validation_NN_experiments.py extra_large 10 00 extrap std TSdraftslopereldGL 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/experiments/extra_large_extrap_std_noisf00_notblock10_TSdraftslopereldGL.log
---
+++ scripts/BASH/JOB_SCRIPTS/experiments/medium_extrap_std_noisf38_notblock00_TSTfdGLdIFwcd.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/run_cross_validation_NN_experiments.py medium 00 38 extrap std TSTfdGLdIFwcd 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/experiments/medium_extrap_std_noisf38_notblock00_TSTfdGLdIFwcd.log
