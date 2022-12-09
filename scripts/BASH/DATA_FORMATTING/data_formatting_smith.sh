b9df4e016a83f91f92f9bfd6cb76bc2417c68779
Clara Burgard <clara.burgard@gmail.com>
2022-12-09 17:24:39 +0100

    worked on initial formatting of Robins data

--- scripts/BASH/DATA_FORMATTING/data_formatting_smith.sh
+++ scripts/BASH/DATA_FORMATTING/data_formatting_smith.sh
@@ -12,66 +12,90 @@ homepath=/bettik/burgardc
 # nemo_run=OPM016
 # nemo_run=OPM018
 # nemo_run=OPM021
-
+# nemo_run=bf663
+nemo_run=bi646
 
 ################# DECLARE THE PATHS ##############################
 path1=$homepath/DATA/NN_PARAM/raw/SMITH_DATA
-path2=$homepath/DATA/NN_PARAM/interim/SMITH_bi646
+path2=$homepath/DATA/NN_PARAM/interim/SMITH_"$nemo_run"
 #path3=$homepath/DATA/BASAL_MELT_PARAM/interim/NEMO_eORCA025.L121_"$nemo_run"_ANT_STEREO
 #path4=$homepath/DATA/BASAL_MELT_PARAM/raw/BEDMACHINE_RTOPO
-path5=$homepath/DATA/NN_PARAM/raw/
+path5=$homepath/DATA/NN_PARAM/raw
 ###################################################################
 
+
+
 ###### VARIABLES
+ncks -d y,0,500 $path5/grid_eORCA025_CDO_Fabien.nc -o $path5/grid_eORCA025_CDO_Fabien_southofaround50.nc
 
 echo 'cp > create gridded file'
-cp $path5/grid_eORCA025_CDO_Fabien.nc $path2/variables_of_interest_allyy.nc # I would need to check if I can cut the grid
+cp $path5/grid_eORCA025_CDO_Fabien_southofaround50.nc $path2/3D_variables_of_interest_allyy.nc 
 
 for var in {thetao,so}
 do
 echo $var
 echo 'ncks > extract variable from gridT' $var
-ncks -O -C -v $var $path1/bi646_1y_grid-T-y300tMX_ANNAVG.nc $path2/$var.nc
+#ncks -O -C -v $var $path1/bi646_1y_grid-T-y300tMX_ANNAVG.nc $path2/$var.nc
+ncks -O -C -v $var $path1/"$nemo_run"_1y_grid-T_AVG.nc $path2/$var.nc
 echo 'ncks > put variable in file' $var
-ncks -A -C -v $var $path2/$var.nc $path2/variables_of_interest_allyy.nc
+ncks -A -C -v $var $path2/$var.nc $path2/3D_variables_of_interest_allyy.nc
 echo 'ncatted > put coords to lon lat' $var
-ncatted -a coordinates,$var,m,c,"lon lat" $path2/variables_of_interest_allyy.nc
+ncatted -a coordinates,$var,m,c,"lon lat" $path2/3D_variables_of_interest_allyy.nc
 \rm $path2/$var.nc
 done
 
+echo 'cp > create gridded file'
+cp $path5/grid_eORCA025_CDO_Fabien_southofaround50.nc $path2/2D_variables_of_interest_allyy.nc 
+
 var=sowflisf
 echo $var
-echo 'ncks > extract variable from flxT' $var
-ncks -O -C -v $var $path1/bi646_1y_isf-T-y300tMX_ANNAVG.nc $path2/$var.nc
+echo 'ncks > extract variable from gridT' $var
+ncks -O -C -v $var $path1/"$nemo_run"_1y_isf-T_AVG.nc $path2/$var.nc
 echo 'ncks > put variable in file' $var
-ncks -A -C -v $var $path2/$var.nc $path2/variables_of_interest_allyy.nc
+ncks -A -C -v $var $path2/$var.nc $path2/2D_variables_of_interest_allyy.nc
 echo 'ncatted > put coords to lon lat' $var
-ncatted -a coordinates,$var,m,c,"lon lat" $path2/variables_of_interest_allyy.nc
+ncatted -a coordinates,$var,m,c,"lon lat" $path2/2D_variables_of_interest_allyy.nc
 \rm $path2/$var.nc
 
+var=tos
+echo $var
+echo 'ncks > extract variable from gridT' $var
+ncks -O -C -v $var $path1/"$nemo_run"_1y_grid-T_AVG.nc $path2/$var.nc
+echo 'ncks > put variable in file' $var
+ncks -A -C -v $var $path2/$var.nc $path2/2D_variables_of_interest_allyy.nc
+echo 'ncatted > put coords to lon lat' $var
+ncatted -a coordinates,$var,m,c,"lon lat" $path2/2D_variables_of_interest_allyy.nc
+\rm $path2/$var.nc
 
 ###### MASK
 
 echo 'cp > create gridded file'
-cp $path5/grid_eORCA025_CDO_Fabien.nc $path2/mask_variables_of_interest_allyy.nc
+cp $path5/grid_eORCA025_CDO_Fabien_southofaround50.nc $path2/mask_variables_of_interest_allyy.nc
 
 # write out the ice-shelf draft and the bathymetry 
 for var in {isf_draft,Bathymetry_isf} 
 do
 echo $var
 echo 'ncks > extract variable'
-ncks -O -C -v $var $path1/bi646c_bathymetry-isf-y300.nc  $path2/$var.nc
+ncks -O -C -v $var $path1/bi646c_YYYY1201_bathymetry-isf.nc  $path2/$var.nc
 echo 'ncks > put variable in file'
 ncks -A -C -v $var $path2/$var.nc $path2/mask_variables_of_interest_allyy.nc
 echo 'ncatted > put coords to lon lat'
 ncatted -a coordinates,$var,m,c,"lon lat" $path2/mask_variables_of_interest_allyy.nc
 done
 
+cdo setgrid,$path2/2D_variables_of_interest_allyy.nc $path2/mask_variables_of_interest_allyy.nc $path2/mask_variables_of_interest_allyy_setgrid.nc
+cdo sellonlatbox,0,360,-90,-50 $path2/mask_variables_of_interest_allyy_setgrid.nc $path2/mask_variables_of_interest_allyy_Ant.nc
+cdo sellonlatbox,0,360,-90,-50 $path2/3D_variables_of_interest_allyy.nc $path2/3D_variables_of_interest_allyy_Ant.nc
+cdo sellonlatbox,0,360,-90,-50 $path2/2D_variables_of_interest_allyy.nc $path2/2D_variables_of_interest_allyy_Ant.nc
+
+
+
 #### PREPARE LAND SEA MASK
 
-cdo sub Bathymetry_isf.nc isf_draft.nc diff_bathy_draft.nc # difference bathymetry - isf draft
-cdo gtc,0 isf_draft.nc isfdraft_gtc0.nc # identify where there is ice => 1
-cdo eqc,0 Bathymetry_isf.nc bathy_0.nc # identify where there is ground without ice => 1
-cdo add bathy_0.nc isfdraft_gtc0.nc ice_1.nc # where there is ice and ground without ice => 1
-cdo eqc,0 diff_bathy_draft.nc diff_0.nc # where there is ice and ground without ice => 1
-cdo add ice_1.nc diff_0.nc lsmask_012.nc # land sea mask with 012
+cdo sub -selvar,Bathymetry_isf $path2/mask_variables_of_interest_allyy_Ant.nc -selvar,isf_draft $path2/mask_variables_of_interest_allyy_Ant.nc  $path2/diff_bathy_draft.nc # difference bathymetry - isf draft
+cdo gtc,0 -selvar,isf_draft $path2/mask_variables_of_interest_allyy_Ant.nc $path2/isfdraft_gtc0.nc # identify where there is ice => 1
+cdo eqc,0 -selvar,Bathymetry_isf $path2/mask_variables_of_interest_allyy_Ant.nc $path2/bathy_0.nc # identify where there is ground without ice => 1
+cdo add $path2/bathy_0.nc $path2/isfdraft_gtc0.nc $path2/ice_1.nc # where there is ice and ground without ice => 1
+cdo eqc,0 $path2/diff_bathy_draft.nc $path2/diff_0.nc # where there is ice and ground without ice => 1
+cdo add $path2/ice_1.nc $path2/diff_0.nc $path2/lsmask_012.nc # land sea mask with 012
