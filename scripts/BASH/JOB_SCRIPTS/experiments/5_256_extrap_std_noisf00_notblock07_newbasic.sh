6927508b078d3d794dfe5871c44ea8d369bd78ba
Clara Burgard <clara.burgard@gmail.com>
2023-03-21 14:32:06 +0100

    gridsearch files

---
+++ scripts/BASH/JOB_SCRIPTS/experiments/5_256_extrap_std_noisf00_notblock07_newbasic.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/run_cross_validation_NN_experiments_gridsearch.py 5 256 07 00 extrap std newbasic 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/experiments/5_256_extrap_std_noisf00_notblock07_newbasic.log
