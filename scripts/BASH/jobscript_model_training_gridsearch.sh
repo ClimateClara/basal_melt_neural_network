3ad9d17b077a27c11ccd90d3a4d016297f37d513
Clara Burgard <clara.burgard@gmail.com>
2023-03-22 12:30:44 +0100

    rapidly checking the gridsearch with relu, gives similar results to lrelu

--- scripts/BASH/jobscript_model_training_gridsearch.sh
+++ scripts/BASH/jobscript_model_training_gridsearch.sh
@@ -3,22 +3,22 @@
 ## RUN CROSS VALIDATION JOBS
 
 layer_nb=5 #'mini', 'small', 'medium', 'large', 'extra_large' # still need L and XL for both CV
-layer_size=96
+layer_size=256
 TS_opt=extrap # extrap, whole, thermocline
 norm_method=std # std, interquart, minmax
 exp_name=newbasic
 
 
-for ii in 10 11 12 13 18 22 23 24 25 30 31 33 38 39 40 42 43 44 45 47 48 51 52 53 54 55 58 61 65 66 69 70 71 73 75 
-#for ii in 73 75
-do
-isf_out=$ii
-#isf_out=00
-
-#for tt in {01..13} # restart at 03 for mini # 13 need to be redone as well
+#for ii in 10 11 12 13 18 22 23 24 25 30 31 33 38 39 40 42 43 44 45 47 48 51 52 53 54 55 58 61 65 66 69 70 71 73 75 # do again from 48 on
+#for ii in 48 51 52 53 54 55 58 61 65 66 69 70 71 73 75
 #do
-#tblock_out=$tt
-tblock_out=00
+#isf_out=$ii
+isf_out=00
+
+for tt in {01..13} # restart at 03 for mini # from 8 need to be redone as well
+do
+tblock_out=$tt
+#tblock_out=00
 
 path_jobscripts=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_SCRIPTS/experiments
 path_outfiles=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/experiments
@@ -36,7 +36,7 @@ EOF
 
 chmod +x $path_jobscripts/${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name}.sh
 
-oarsub -S -n ${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name} --stdout $path_jobid/${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name}.o%jobid%  --stderr $path_jobid/${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name}.e%jobid% -l nodes=1/core=4,walltime=10:00:00 --project mais -p "network_address='luke62'" $path_jobscripts/${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name}.sh
+oarsub -S -n ${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name} --stdout $path_jobid/${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name}.o%jobid%  --stderr $path_jobid/${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name}.e%jobid% -l nodes=1/core=4,walltime=05:00:00 --project mais -p "network_address='luke62'" $path_jobscripts/${layer_nb}_${layer_size}_${TS_opt}_${norm_method}_noisf${isf_out}_notblock${tblock_out}_${exp_name}.sh
 
 # to remove if no CV!!!
 done
