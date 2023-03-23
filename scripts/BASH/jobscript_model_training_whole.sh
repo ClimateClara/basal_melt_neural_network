4ef0f0293cc54dbfb557e11a7d3354591f4ec65c
Clara Burgard <clara.burgard@gmail.com>
2023-03-23 19:54:37 +0100

    prepared tuning on whole dataset scripts using a seed

--- scripts/BASH/jobscript_model_training_whole.sh
+++ scripts/BASH/jobscript_model_training_whole.sh
@@ -6,6 +6,7 @@ mod_size=large  #'mini', 'small', 'medium', 'large', 'extra_large'
 TS_opt=extrap # extrap, whole, thermocline
 norm_method=std # std, interquart, minmax
 exp_name=newbasic
+seed_nb=01
 
 
 path_jobscripts=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_SCRIPTS/experiments
@@ -14,17 +15,17 @@ path_outfiles=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/J
 path_python=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON
 path_jobid=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/JOB_STD_OUTPUT
 
-cat <<EOF > $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}.sh 
+cat <<EOF > $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.sh 
 
 #!/bin/bash
 
 conda activate neuralnet
-python -u $path_python/run_training_whole_dataset.py ${mod_size} ${TS_opt} ${norm_method} ${exp_name} 2>&1 | tee $path_outfiles/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}.log
+python -u $path_python/run_training_whole_dataset.py ${mod_size} ${TS_opt} ${norm_method} ${exp_name} ${seed_nb} 2>&1 | tee $path_outfiles/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.log
 EOF
 
-chmod +x $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}.sh
+chmod +x $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.sh
 
-oarsub -S -n ${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name} --stdout $path_jobid/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}.o%jobid%  --stderr $path_jobid/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}.e%jobid% -l nodes=1/core=6,walltime=10:00:00 --project mais -p "network_address='luke62'" $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}.sh
+oarsub -S -n ${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb} --stdout $path_jobid/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.o%jobid%  --stderr $path_jobid/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.e%jobid% -l nodes=1/core=6,walltime=10:00:00 --project mais -p "network_address='luke62'" $path_jobscripts/${mod_size}_${TS_opt}_${norm_method}_wholedataset_${exp_name}_${seed_nb}.sh
 
 # to remove if no CV!!!
 
