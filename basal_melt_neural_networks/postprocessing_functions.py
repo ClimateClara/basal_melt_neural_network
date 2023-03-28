63996f0b170a00a9ac25bba3f1a91f2f4c9c64d8
Clara Burgard <clara.burgard@gmail.com>
2023-03-28 18:32:28 +0200

    applied the deep ensemble to data

--- basal_melt_neural_networks/postprocessing_functions.py
+++ basal_melt_neural_networks/postprocessing_functions.py
@@ -101,14 +101,14 @@ def apply_NN_results_2D_1isf_1tblock(file_isf, norm_metrics, df_nrun, model, inp
     return y_whole_grid
 
     
-def evalmetrics_1D_NN(kisf, norm_metrics, df_nrun, model, file_isf, geometry_info_2D, box_charac_2D, box_charac_1D, isf_stack_mask, input_vars=[]):
+def evalmetrics_1D_NN(kisf, norm_metrics, df_nrun, model, file_isf, geometry_info_2D, box_charac_2D, box_charac_1D, isf_stack_mask, input_vars=[], ensemble_mean=False):
     
     """
     Compute 1D metrics based on a given NN model
     
     """
-    
-    melt2D = apply_NN_results_2D_1isf_1tblock(file_isf, norm_metrics, df_nrun, model, input_vars)
+
+    melt2D = apply_NN_results_2D_1isf_1tblock(file_isf, norm_metrics, df_nrun, model, input_vars, ensemble_mean)
 
     if box_charac_2D and box_charac_1D:
         box_loc_config2 = box_charac_2D['box_location'].sel(box_nb_tot=box_charac_1D['nD_config'].sel(config=2))
@@ -313,3 +313,42 @@ def compute_crossval_metric_2D_for_1CV(tblock_out,isf_out,tblock_dim,isf_dim,inp
                 res_2D_all = res_2D_all.combine_first(res_2D)
             
     return res_2D_all
+
+def read_input_evalmetrics_NN_yy(nemo_run, tt, file_conc, file_other):
+    inputpath_boxes = '/bettik/burgardc/DATA/NN_PARAM/interim/BOXES/SMITH_'+nemo_run+'/'
+    inputpath_data='/bettik/burgardc/DATA/NN_PARAM/interim/SMITH_'+nemo_run+'/'
+    inputpath_mask='/bettik/burgardc/DATA/NN_PARAM/interim/ANTARCTICA_IS_MASKS/SMITH_'+nemo_run+'/'
+    
+    file_isf_orig = xr.open_dataset(inputpath_mask+'nemo_5km_isf_masks_and_info_and_distance_oneFRIS_'+str(tt)+'.nc').drop('time')
+    #file_isf = file_isf_orig.sel(Nisf=isf_list)
+    nonnan_Nisf = file_isf_orig['Nisf'].where(np.isfinite(file_isf_orig['front_bot_depth_max']), drop=True).astype(int)
+    file_isf_nonnan = file_isf_orig.sel(Nisf=nonnan_Nisf)
+    large_isf = file_isf_nonnan['Nisf'].where(file_isf_nonnan['isf_area_here'] >= 2500, drop=True)
+    file_isf = file_isf_nonnan.sel(Nisf=large_isf)
+    if 'labels' in file_isf.coords.keys():
+        file_isf = file_isf.drop('labels')
+    
+    file_other_cut = file_other.sel(time=tt)
+    file_conc_cut = file_conc.sel(time=tt)
+
+    ice_draft_pos = file_other_cut['corrected_isfdraft']
+    
+    box_charac_2D = xr.open_dataset(inputpath_boxes + 'nemo_5km_boxes_2D_oneFRIS_'+str(tt)+'_merged75.nc')
+    box_charac_1D = xr.open_dataset(inputpath_boxes + 'nemo_5km_boxes_1D_oneFRIS_'+str(tt)+'_merged75.nc')
+    
+    isf_stack_mask = dfmt.create_stacked_mask(file_isf['ISF_mask'], file_isf.Nisf, ['y','x'], 'mask_coord')
+
+    file_isf_conc = file_conc_cut['isfdraft_conc']
+
+    xx = file_isf.x
+    yy = file_isf.y
+    dx = (xx[2] - xx[1]).values
+    dy = (yy[2] - yy[1]).values
+    grid_cell_area = abs(dx*dy)  
+    grid_cell_area_weighted = file_isf_conc * grid_cell_area
+    
+    geometry_info_2D = xr.merge([ice_draft_pos.rename('ice_draft_pos'),
+                            grid_cell_area_weighted.rename('grid_cell_area_weighted'),
+                            file_isf_conc])
+    
+    return file_isf, geometry_info_2D, box_charac_2D, box_charac_1D, isf_stack_mask
