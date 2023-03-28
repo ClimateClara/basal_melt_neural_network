25e78c9b26aa20eb978fc2d07fd47193b13386ac
Clara Burgard <clara.burgard@gmail.com>
2023-03-28 18:34:09 +0200

    worked on script for shuffling, also to send as job, now waiting to decide on which architecture I do it

---
+++ scripts/BASH/jobscript_model_apply_1D_ensmean_shuffling.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+## RUN CROSS VALIDATION JOBS
+
+mod_size=small  #'mini', 'small', 'medium', 'large', 'extra_large'
+TS_opt=extrap # extrap, whole, thermocline
+norm_method=std # std, interquart, minmax
+exp_name=newbasic2
+nemo_run=bf663
+
+for vv in dGL dIF #corrected_isfdraft bathy_metry slope_bed_lon slope_bed_lat slope_ice_lon slope_ice_lat theta_in salinity_in T_mean S_mean T_std S_std
+do 
+
+
+path_jobscripts=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_SCRIPTS/apply_2D_and_1D
+path_outfiles=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/apply_2D_and_1D
+
+path_python=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON
+path_jobid=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/JOB_STD_OUTPUT
+
+cat <<EOF > $path_jobscripts/shuffling_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.sh 
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u $path_python/compute_1D_evalmetrics_shuffling_deepensemble_Smith.py ${mod_size} ${TS_opt} ${norm_method} ${exp_name} ${nemo_run} ${vv} 2>&1 | tee $path_outfiles/shuffling_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.log
+EOF
+
+chmod +x $path_jobscripts/shuffling_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.sh
+
+oarsub -S -n shuffling_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run} --stdout $path_jobid/shuffling_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.o%jobid%  --stderr $path_jobid/shuffling_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.e%jobid% -l nodes=1/core=4,walltime=12:00:00 --project mais -p "network_address='luke62'" $path_jobscripts/shuffling_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.sh
+
+# to remove if no CV!!!
+
+done
+
+
+
+
+
+
