2561aa1e3f01bbba23510c013f5d750d05c29d26
Clara Burgard <clara.burgard@gmail.com>
2023-10-11 17:47:03 +0200

    sent shuffling experiments to get 2D results

---
+++ scripts/BASH/jobscript_model_apply_2D_ensmean_shuffling.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## RUN CROSS VALIDATION JOBS
+
+mod_size=small #'mini', 'small', 'medium', 'large', 'extra_large'
+TS_opt=extrap_shuffboth # extrap, whole, thermocline #extrap_shuffboth
+norm_method=std # std, interquart, minmax
+exp_name=newbasic2
+nemo_run=bf663
+
+for vv in dGL dIF corrected_isfdraft bathy_metry slope_bed_lon slope_bed_lat slope_ice_lon slope_ice_lat theta_in salinity_in T_mean S_mean T_std S_std watercolumn position slopesbed slopesice Tinfo Sinfo
+do 
+
+
+path_jobscripts=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_SCRIPTS/apply_2D_and_1D
+path_outfiles=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/apply_2D_and_1D
+
+path_python=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON
+path_jobid=/bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/JOB_STD_OUTPUT
+
+cat <<EOF > $path_jobscripts/shuffling2D_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.sh 
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u $path_python/compute_2D_evalmetrics_shuffling_deepensemble_Smith.py ${mod_size} ${TS_opt} ${norm_method} ${exp_name} ${nemo_run} ${vv} 2>&1 | tee $path_outfiles/shuffling2D_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.log
+EOF
+
+chmod +x $path_jobscripts/shuffling2D_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.sh
+
+oarsub -S -n shuffling2D_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run} --stdout $path_jobid/shuffling2D_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.o%jobid%  --stderr $path_jobid/shuffling2D_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.e%jobid% -l nodes=1/core=3,walltime=08:00:00 --project mais -p "network_address='luke62'" $path_jobscripts/shuffling2D_${vv}_ensmean_${mod_size}_${TS_opt}_${norm_method}_${exp_name}_${nemo_run}.sh
+
+# to remove if no CV!!!
+
+done
+
+
+
