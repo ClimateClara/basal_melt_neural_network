7ffa82b9e7467d23669a13ed60a1b5a1868c85fc
Clara Burgard <clara.burgard@gmail.com>
2024-01-11 15:54:43 +0100

    new shuffling (deep ensemble) runscripts

---
+++ scripts/BASH/JOB_SCRIPTS/apply_2D_and_1D/shuffling2D_dGL_ensmean_small_extrap_shuffboth_std_newbasic2_bf663.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/compute_2D_evalmetrics_shuffling_deepensemble_Smith.py small extrap_shuffboth std newbasic2 bf663 dGL 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/apply_2D_and_1D/shuffling2D_dGL_ensmean_small_extrap_shuffboth_std_newbasic2_bf663.log
---
+++ scripts/BASH/JOB_SCRIPTS/apply_2D_and_1D/shuffling2D_slope_bed_lat_ensmean_small_extrap_shuffboth_std_newbasic2_bf663.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+conda activate neuralnet
+python -u /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/PYTHON/compute_2D_evalmetrics_shuffling_deepensemble_Smith.py small extrap_shuffboth std newbasic2 bf663 slope_bed_lat 2>&1 | tee /bettik/burgardc/SCRIPTS/basal_melt_neural_networks/scripts/BASH/JOB_OUTFILES/apply_2D_and_1D/shuffling2D_slope_bed_lat_ensmean_small_extrap_shuffboth_std_newbasic2_bf663.log
